6363ead08c42737688278a41287c01206e288053
Ervin Montoya <ervin.montoya@softwareone.com>
2023-11-27 14:36:17 -0500

    primera carga

---
+++ SumaDosNumeros/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SumaDosNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese el primer número:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            int suma = num1 + num2;
+            Console.WriteLine($"La suma de los dos números es: {suma}");
+
+            Console.ReadLine();
+        }
+    }
+}
